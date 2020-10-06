273660e8b38c6d2612f9e55c754b1b7979ae3ed3
Orlov Artem Sergeevich <artm.orlov.88@list.ru>
2020-10-06 13:42:04 +0500

    feat: decide task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let a = str;
+
+  for (let i = 0; i <= str.length - 1; i++) {
+    for (let elem of bracketsConfig) {
+      a = a.replace(elem.join(''), '');
+
+      if (a.length == 0) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
